ca2c90c5c549225267cffeb4bf486fc911a9c0c9
noonett <709387417@qq.com>
2020-06-03 16:43:53 +0800

    upload third demo

---
+++ DaoDemo/community/src/test/java/com/nowcoder/community/MapperTests.java
@@ -0,0 +1,55 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Date;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTests {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelectUser(){
+        User user = userMapper.selectById(101);
+        System.out.println(user);
+
+        user = userMapper.selectByName("liubei");
+        System.out.println(user);
+
+        user = userMapper.selectByEmail("nowcoder101@sina.com");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testInsertUser(){
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("123456");
+        user.setSalt("abcf1");
+        user.setEmail("test@qq.com");
+        user.setHeaderUrl("http://images.nowcoder.com/head/100t.png");
+        user.setCreateTime(new Date());
+
+        int rows = userMapper.insertUser(user);
+        System.out.println(rows);
+        System.out.println(user.getId());
+    }
+
+    @Test
+    public void updateUser(){
+        int rows = userMapper.updateStatus(150, 1);
+        System.out.println(rows);
+        rows = userMapper.updateHeader(150, "http://images.nowcoder.com/head/21t.png");
+        System.out.println(rows);
+        rows = userMapper.updatePassword(150, "hihi");
+        System.out.println(rows);
+    }
+}
